6f912a0e37cbd61569ee6aa04153789fcc2b3942
Saeid Md <khanrajon5@gmail.com>
2024-01-23 14:44:47 +0200

    array 2 exercise value added

--- array2.php
+++ array2.php
@@ -10,3 +10,17 @@ $sales2024 = [
     "Feb" => 0,
     "Mar" => null,
 ];
+
+$sales2024 = [
+    "Jan" => 10,
+    "Feb" => null,
+    "Mar" => null,
+];
+
+$sales = [2023 => $sales2023, 2024 => $sales204];
+var_dump($sales);
+
+print $sales[2023]["Jan"];
+print "\n";
+$sales[2024]["Jan"] = $sales[2024]["Jan"] + 42;
+print $sales[2024]["Jan"];
