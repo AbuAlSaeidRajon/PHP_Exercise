86addd6a6064ef019e7abe9db61dcefaa5163a15
Saeid Md <khanrajon5@gmail.com>
2024-01-23 13:12:23 +0200

    age printed

--- firstFunction.php
+++ firstFunction.php
@@ -29,11 +29,3 @@ function helloPerson ($name) {
 
 helloPerson("Hales");
 helloPerson("Alex");
-
-// Write a function called checkAge, which will return true if age is equal or over 18. otherwise it returns false. 
-// examples: 
-
-checkAge(25); //this should return true
-checkAge(-2); //this shouldn return false
-checkAge(12); // -"- false 
-checkAge(12345678); // -"- true 
--- functionExercise1.php
+++ functionExercise1.php
@@ -1,12 +1,35 @@
+<!-- Write a function called checkAge, which will return true if age is equal or over 18. otherwise it returns false.  -->
+<!-- examples:  -->
+<!-- checkAge(25); //this should return true -->
+<!-- checkAge(-2); //this should return false -->
+<!-- checkAge(12); // -"- false  -->
+<!-- checkAge(12345678); // -"- true  -->
+
+
 <?php
 function checkAge($age) {
-    return $age >= 18;
+    if ($age >= 18) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+$age1 = 25;
+$result1 = checkAge($age1);
+print "checkAge($age1) returns " . ($result1 ? 'true' : 'false') . "\n";
 
-var_dump(checkAge(25));       
-var_dump(checkAge(-2));       
-var_dump(checkAge(12));       
-var_dump(checkAge(12345678)); 
+$age2 = -2;
+$result2 = checkAge($age2);
+print "checkAge($age2) returns " . ($result2 ? 'true' : 'false') . "\n";
+
+$age3 = 12;
+$result3 = checkAge($age3);
+print "checkAge($age3) returns " . ($result3 ? 'true' : 'false') . "\n";
+
+$age4 = 12345678;
+$result4 = checkAge($age4);
+print "checkAge($age4) returns " . ($result4 ? 'true' : 'false') . "\n";
 ?>
+
 
