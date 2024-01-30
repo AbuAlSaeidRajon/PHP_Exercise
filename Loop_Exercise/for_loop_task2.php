a7be75b09073e5ac68af87475f9920e4b3814d92
Saeid Md <khanrajon5@gmail.com>
2024-01-30 11:02:09 +0200

    for loop task 2 added

---
+++ Loop_Exercise/for_loop_task2.php
@@ -0,0 +1,4 @@
+// Make a program that calculates and prints the sum of the series from $number1 to $number2. Give values of your choosing to these variables (in real-life case they’d most likely come from a form in web page). E.g. if the values are 1 and 5, the program should calculate 1+2+3+4+5 and print the value 15.
+
+**Extra challenge: Print the calculation too. You may need a conditional statement (if) to avoid printing an extra plus sign to the end.
+
