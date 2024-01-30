8536bd90ab38ae54437ade23753df461b5e33a4d
Saeid Md <khanrajon5@gmail.com>
2024-01-30 11:01:03 +0200

    for loop 1 task done

--- Loop_Exercise/for_loop_task1.php
+++ Loop_Exercise/for_loop_task1.php
@@ -1,3 +1,9 @@
 // Make a program that prints all positive numbers that are even and smaller than 100 starting from 2. (2 4 6 8 10...)
 
 <?php
+$i = 2;
+
+while ($i < 100) {
+    print $i . " ";
+    $i += 2;  
+}
