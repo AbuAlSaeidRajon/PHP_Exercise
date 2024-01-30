893298dccb7576de68f507630ec0aa4182af96b4
Saeid Md <khanrajon5@gmail.com>
2024-01-30 09:52:44 +0200

    foreach function added

--- Loop_Exercise/foreach_loop.php
+++ Loop_Exercise/foreach_loop.php
@@ -2,6 +2,10 @@
 
 $colors = array("red", "blue", "green", "yellow");
 
-for($i = 0; i < count($colors); $i++) {
+/*for($i = 0; i < count($colors); $i++) {
     print $colors[$i] . "\n";
+} */
+
+foreach($colors as $color) {
+    print $color;
 }
