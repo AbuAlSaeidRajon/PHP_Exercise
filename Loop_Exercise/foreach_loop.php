48aa1a8e25811c3e96b8453f8e7042b89dbbffe8
Saeid Md <khanrajon5@gmail.com>
2024-01-30 09:54:53 +0200

    comment added

--- Loop_Exercise/foreach_loop.php
+++ Loop_Exercise/foreach_loop.php
@@ -7,5 +7,6 @@ $colors = array("red", "blue", "green", "yellow");
 } */
 
 foreach($colors as $color) {
+// for each item in "$colors", we know them as $color (one at a time)    
     print $color;
 }
