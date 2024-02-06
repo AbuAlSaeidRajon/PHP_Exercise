465c0cf87f2dd8fac76ec624f0b2f8ee42bf2369
Saeid Md <khanrajon5@gmail.com>
2024-02-06 12:14:07 +0200

    question added

---
+++ The_form_code/task2.php
@@ -0,0 +1,9 @@
+<?php
+
+        // Check that all the fields are filled and that the passwords match
+
+        // If everything is ok show a personalized Welcome message
+
+        // If something is wrong show a link back to the form
+
+
