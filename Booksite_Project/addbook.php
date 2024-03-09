1056f0f74b20d47db4a70961cdbe480ef65557ac
Saeid Md <khanrajon5@gmail.com>
2024-03-09 15:23:51 +0200

    html file added

--- Booksite_Project/addbook.php
+++ Booksite_Project/addbook.php
@@ -39,3 +39,49 @@
         exit;
     }
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Favorite Books</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="booksite.css">
+</head>
+<body>
+    <div id="container">
+        <header>
+            <h1>Your Favorite Books</h1>
+        </header>
+        <nav id="main-navi">
+            <ul>
+                <li><a href="booksite.php">Home</a></li>
+                <li><a href="booksite.php?category=adventure">Adventure</a></li>
+                <li><a href="booksite.php?category=classic">Classic Literature</a></li>
+                <li><a href="booksite.php?category=coming-of-age">Coming-of-age</a></li>
+                <li><a href="booksite.php?category=fantasy">Fantasy</a></li>
+                <li><a href="booksite.php?category=historical">Historical Fiction</a></li>
+                <li><a href="booksite.php?category=horror">Horror</a></li>
+                <li><a href="booksite.php?category=mystery">Mystery</a></li>
+                <li><a href="booksite.php?category=romance">Romance</a></li>
+                <li><a href="booksite.php?category=scifi">Science Fiction</a></li>
+            </ul>
+        </nav>
+        <main>
+            <form action="login.php" method="post">
+                <p>
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username">
+                </p>
+                <p>
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password">
+                </p>
+                <p><input type="submit" name="login" value="Log in"></p>
+            </form>
+        </main>
+    </div>    
+</body>
+</html>
