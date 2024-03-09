ed651e5944733972ac055564c5f6622eac271ac4
Saeid Md <khanrajon5@gmail.com>
2024-03-09 15:20:30 +0200

    loggedin option added

---
+++ Booksite_Project/addbook.php
@@ -0,0 +1,7 @@
+<?php
+    // If the user is not logged in, redirect them back to login.php.
+    session_start();
+    if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+        header("location: login.php");
+        exit;
+    }
