ab511a2fe4a4ce10cf868425e9e079e3e0915cff
Saeid Md <khanrajon5@gmail.com>
2024-03-09 15:22:23 +0200

    book added to the array

--- Booksite_Project/addbook.php
+++ Booksite_Project/addbook.php
@@ -5,3 +5,26 @@
         header("location: login.php");
         exit;
     }
+
+    // If the form has been sent, add the book to the data file
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // In order to protect against cross-site scripting attacks (i.e. basic PHP security), remove HTML tags from all input.
+        $title = strip_tags($_POST["title"]);
+        $author = strip_tags($_POST["author"]);
+        $year = strip_tags($_POST["year"]);
+        $genre = strip_tags($_POST["genre"]);
+        $description = strip_tags($_POST["description"]);
+
+        // Read the file into array variable $books
+        $json = file_get_contents("books.json");
+        $books = json_decode($json, true);
+
+         // Add the new book to the array
+         $newBook = [
+            "bookid" => $_POST["bookid"],
+            "title" => $title,
+            "author" => $author,
+            "year" => $year,
+            "genre" => $genre,
+            "description" => $description
+        ];
